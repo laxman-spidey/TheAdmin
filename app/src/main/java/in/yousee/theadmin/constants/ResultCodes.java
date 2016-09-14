9e01b17c5d119f40b4d962c2fb36d87d7c1cc5b9
Rashmitha <rashmithadevulapally@gmail.com>
2016-09-14 08:53:53 +0530

    constants added for attendance and authrorization features

--- app/src/main/java/in/yousee/theadmin/constants/ResultCodes.java
+++ app/src/main/java/in/yousee/theadmin/constants/ResultCodes.java
@@ -11,6 +11,41 @@ public class ResultCodes {
     public static final int NETWORK_VALIDATE_OTP_UPDATE_FAILED = 707;
     public static final int NETWORK_VALIDATE_OTP_INVALID = 708;
 
+    public static final int CHECKIN_ALREADY_CHECKEDIN = 101;
+    public static final int CHECKIN_SUCCESS = 102;
+    public static final int CHECKIN_INSERT_DBERROR = 103;
+    public static final int INFO_WEEKOFF = 104;
+    public static final int WARNING_ROASTER_DOES_NOT_EXIST = 105;
+
+    public static final int CHECKOUT_SUCCESS = 111;
+    public static final int CHECKOUT_INSERT_DBERROR = 112;
+    public static final int CHECKOUT_NOT_CHECKEDIN = 113;
+
+    public static final int ROASTER_DETAILS_NOT_EXIST = 121;
+    public static final int ROASTER_DETAILS_EXIST = 122;
+
+    public static final int ATTENDANCE_HISTORY_NOT_EXIST = 131;
+    public static final int ATTENDANCE_HISTORY_EXIST = 132;
+
+    public static final int CHECK_AUTHORIZATION_SUCCESS = 141;
+    public static final int CHECK_AUTHORIZATION_FAIL = 142;
+    public static final int CHECK_AUTHORIZATION_EXPIRATION_UPDATE_FAIL = 143;
+
+    public static final int LOGIN_SUCCESS = 151;
+    public static final int USER_DATA_SUCCESS = 152;
+    public static final int USER_DATA_FAIL = 153;
+
+    public static final int VALIDATE_OTP_STATUS_UPDATE_SUCCESS = 161;
+    public static final int VALIDATE_OTP_STATUS_UPDATE_FAIL = 162;
+    public static final int INVALID_OTP = 163;
+
+
+
+
+
+
+
+
 
 
 
